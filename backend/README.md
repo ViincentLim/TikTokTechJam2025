2421432da1ee030b7d813fc1b93936c80c128794
vincentlim <vincentlimjingen@gmail.com>
2025-08-29 22:11:02 +0800

    Add FastAPI setup and captcha generation endpoint

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# Develop
+Set up .env in this backend directory:
+```env
+GEMINI_API_KEY=<your_api_key>
+```
+Run the development server in /backend with:
+```shell
+uv run --env-file .env fastapi dev
+```
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from google import genai
+from pydantic import BaseModel
+from starlette.responses import HTMLResponse
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/video/{video_id}")
+async def get_video(video_id: str):
+    # Simulate fetching video data
+    return {"video_id": video_id, "title": "Sample Video"}
+
+
+class CaptchaWebsite(BaseModel):
+    thoughts: str
+    html: str
+
+client = genai.Client()
+# Relative filepath ./prompts/generate_captcha.txt
+with open("./prompts/generate_captcha.txt", "r") as f:
+    prompt = f.read()
+
+@app.get("/captcha", response_class=HTMLResponse)
+async def get_captcha():
+    print("Generating captcha...")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+    )
+    print("Captcha generated.")
+    return HTMLResponse(response.text)
