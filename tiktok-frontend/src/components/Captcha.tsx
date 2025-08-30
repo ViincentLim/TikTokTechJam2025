a994d993c2c37dd6cb691bd1ee98ca4af0077983
vincentlim <vincentlimjingen@gmail.com>
2025-08-30 18:08:29 +0800

    captcha setsuccess starts with null

--- tiktok-frontend/src/components/Captcha.tsx
+++ tiktok-frontend/src/components/Captcha.tsx
@@ -1,14 +1,13 @@
-import WebView from "./native-elements/WebView.js";
 import {useState} from '@lynx-js/react';
 
 export default function Captcha() {
-    let [success, setSuccess] = useState(false);
+    let [success, setSuccess] = useState<boolean | null>(null);
 
     return (
         <view style={{
             width: '100%',
             height: '100%',
-            background: success ? "#00ffff88" : "#ff000088",
+            background: success ? '#00ffff88' : '#ff000088',
         }}>
             <text>Solve the captcha</text>
             {/* @ts-ignore - web-view is a valid Lynx custom element*/}
@@ -16,10 +15,10 @@ export default function Captcha() {
                 width: '100%',
                 height: '100%',
             }} url="http://localhost:9090/"
-                bindgameover={setSuccess}
+                      bindgameover={setSuccess}
             >
-            {/* @ts-ignore - web-view is a valid Lynx custom element*/}
+                {/* @ts-ignore - web-view is a valid Lynx custom element*/}
             </web-view>
         </view>
-    )
+    );
 }
